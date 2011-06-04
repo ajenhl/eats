6b1fe5ac63edaa5e8e8bcfe70d3fdf5ba795c012
jamie@artefact.org.nz <jamie@artefact.org.nz@e9a79ef8-3d52-0410-86c1-3742fafee21a>
2011-06-04 09:52:53 +0000

    Added names and entity relationships to administration interface.
    git-svn-id: http://eats.googlecode.com/svn/branches/django-tm/@546 e9a79ef8-3d52-0410-86c1-3742fafee21a

--- server/eats/urls.py
+++ server/eats/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
-from eats.constants import AUTHORITY_TYPE_IRI, ENTITY_TYPE_TYPE_IRI, LANGUAGE_TYPE_IRI, SCRIPT_TYPE_IRI
+from eats.constants import AUTHORITY_TYPE_IRI, ENTITY_RELATIONSHIP_TYPE_IRI, ENTITY_TYPE_TYPE_IRI, LANGUAGE_TYPE_IRI, NAME_TYPE_TYPE_IRI, SCRIPT_TYPE_IRI
 
 
 # Displaying.
@@ -19,8 +19,11 @@ urlpatterns += patterns(
 
 topic_data = {
     'authority': {'type_iri': AUTHORITY_TYPE_IRI, 'name': 'authority'},
+    'entity_relationship_type': {'type_iri': ENTITY_RELATIONSHIP_TYPE_IRI,
+                                 'name': 'entity-relationship-type'},
     'entity_type': {'type_iri': ENTITY_TYPE_TYPE_IRI, 'name': 'entity-type'},
     'language': {'type_iri': LANGUAGE_TYPE_IRI, 'name': 'language'},
+    'name_type': {'type_iri': NAME_TYPE_TYPE_IRI, 'name': 'name-type'},
     'script': {'type_iri': SCRIPT_TYPE_IRI, 'name': 'script'},
     }
 
@@ -36,6 +39,15 @@ urlpatterns += patterns(
         name='authority-add'),
     url(r'^administer/authority/(?P<topic_id>\d+)/$', 'topic_change',
         topic_data['authority'], name='authority-change'),
+    url(r'^administer/entity-relationship-type/$', 'topic_list',
+        topic_data['entity_relationship_type'],
+        name='entity-relationship-type-list'),
+    url(r'^administer/entity-relationship-type/add/$', 'topic_add',
+        topic_data['entity_relationship_type'],
+        name='entity-relationship-type-add'),
+    url(r'^administer/entity-relationship-type/(?P<topic_id>\d+)/$',
+        'topic_change', topic_data['entity_relationship_type'],
+        name='entity-relationship-type-change'),
     url(r'^administer/entity-type/$', 'topic_list', topic_data['entity_type'],
         name='entity-type-list'),
     url(r'^administer/entity-type/add/$', 'topic_add',
@@ -48,6 +60,12 @@ urlpatterns += patterns(
         name='language-add'),
     url(r'^administer/language/(?P<topic_id>\d+)/$', 'topic_change',
         topic_data['language'], name='language-change'),
+    url(r'^administer/name-type/$', 'topic_list', topic_data['name_type'],
+        name='name-type-list'),
+    url(r'^administer/name-type/add/$', 'topic_add',
+        topic_data['name_type'], name='name-type-add'),
+    url(r'^administer/name-type/(?P<topic_id>\d+)/$', 'topic_change',
+        topic_data['name_type'], name='name-type-change'),
     url(r'^administer/script/$', 'topic_list', topic_data['script'],
         name='script-list'),
     url(r'^administer/script/add/$', 'topic_add', topic_data['script'],
