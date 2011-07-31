0168481bfed4092c2c3b9c630c581ab06b47a66b
jamie@artefact.org.nz <jamie@artefact.org.nz@e9a79ef8-3d52-0410-86c1-3742fafee21a>
2011-07-31 09:13:36 +0000

    Corrected mix-up with test files.
    git-svn-id: http://eats.googlecode.com/svn/branches/django-tm/@642 e9a79ef8-3d52-0410-86c1-3742fafee21a

--- server/eats/tests/views/view_test_case.py
+++ server/eats/tests/views/view_test_case.py
@@ -1,6 +1,12 @@
 from django_webtest import WebTest
 
+from eats.tests.base_test_case import BaseTestCase
 
-class ViewTestCase (WebTest):
 
-    pass
+class ViewTestCase (WebTest, BaseTestCase):
+
+    def setUp (self):
+        # Create a topic map.
+        self.tm = self.create_topic_map()
+        # Create an authority.
+        self.authority = self.create_authority('Test')
